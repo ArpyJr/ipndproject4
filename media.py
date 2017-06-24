0c92f0de41546ed2c609fb7b334b6237e15b046b
Jooyong Chung <jychung0801@gmail.com>
2017-06-23 22:20:56 -0500

    made changes to parent class Video

--- media.py
+++ media.py
@@ -2,10 +2,9 @@ import webbrowser
 
 class Video():
 """this is the parent class for classes Movie and TV"""
-	def __init__(self, title, duration, release_date, trailer_url):
+	def __init__(self, title, image_url, trailer_url):
 		self.title = title
-		self.duration = duration
-		self.release_date = release_date
+		self.image_url = image_url
 		self.trailer_url = trailer_url
 
 	def play_trailer():
@@ -14,13 +13,20 @@ class Video():
 
 class Movie(Video):
 """this is child class of Video for movies"""
-	def __init__(self, title, duration, release_date, trailer_url,	box_office):
-		Video.__init__(self, title, duration, release_date, trailer_url)
+	print "Movie constructor Called"
+	def __init__(self, title, image_url, trailer_url, box_office, release_date, duration):
+		Video.__init__(self, title, image_url, trailer_url)
 		self.box_office = box_office
+		self.release_date = release_date
+		self.duration = duration
 
 class TV(Video):
 """this is child class of Video for TV shows"""
-	def __init__(self, title, duration, release_date, trailer_url, num_seasons, num_episodes):
-		Video.__init__(self, title, duration, release_date, trailer_url)
+	print "TV constructor Called"
+	def __init__(self, title, image_url, trailer_url, num_seasons, num_episodes,
+				 first_episode, last_episode):
+		Video.__init__(self, title, image_url, trailer_url)
 		self.num_seasons = num_seasons
 		self.num_episodes = num_episodes
+		self.first_episode = first_episode
+		self.last_episode = last_episode
