73dcca999a486a9b9686ab94377684eb7d6c1559
Jooyong Chung <jychung0801@gmail.com>
2017-06-28 00:04:23 -0500

    done, somehow

---
+++ constructor.py
@@ -0,0 +1,225 @@
+import os
+import webbrowser
+import re
+
+
+html_head = '''
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Favorite Movies and TVs</title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script>
+		window.onload = function () {
+			$(".poster").mouseenter(function() {
+				console.log("mouse entered");
+				$(this).css("display", "none");
+				$(this).siblings(".description").css("display", "block");
+			});
+			$(".description").mouseleave(function() {
+				console.log("mouse left");
+				$(this).css("display", "none");
+				$(this).siblings(".poster").css("display", "block");
+			});
+			$("#video-box").click(function() {
+				$(this).css("display", "none");
+				$(this).empty();
+			});
+		}
+
+		function launchTrailer(url) {
+			$("#video-box").append('<iframe width="560" height="315" src=' + url + ' frameborder="0" allowfullscreen></iframe>');
+			$("#video-box").css("display", "block");
+		}
+		</script>
+		<style>
+			body {
+				width: 100%;
+				height: 100%;
+				margin: 0;
+			}
+
+			#content-box {
+				display: flex;
+				flex-wrap: wrap;
+				margin: 0;
+				height: 100%;
+				width: 100%;
+			}
+
+			.content {
+				margin: 15px auto 15px auto;
+				width: 400px;
+				height: 600px;
+				-webkit-box-shadow: 10px 10px 41px -13px rgba(0,0,0,0.75);
+				-moz-box-shadow: 10px 10px 41px -13px rgba(0,0,0,0.75);
+				box-shadow: 10px 10px 41px -13px rgba(0,0,0,0.75);
+			}
+
+			.poster {
+				height: 100%;
+				width: 100%;
+			}
+
+			.description {
+				display: none;
+				width: 100%;
+				height: 100%;
+			}
+
+			#video-box {
+				position: absolute;
+				top: 0;
+				display: none;
+				z-index: 9;
+				background: rgba(0, 0, 0, 0.70);
+				height: 100%;
+				width: 100%;
+			}
+
+			iframe {
+				margin-left: calc(100%/2 - 560px/2);
+				margin-top: 200px;
+			}
+		</style>
+	</head>
+'''
+
+html_body = '''
+	<body>
+		<div id="content-box">
+			<!--
+			This is the format for the content that will be displayed.
+			Number of div.content should depend on number of Movie and TV instances.
+			<div class="content">
+			</div>
+			-->
+			<!-- example for Movie instance
+			<div class="content">
+				<img class="poster" src="https://s-media-cache-ak0.pinimg.com/564x/fd/5e/66/fd5e662dce1a3a8cd192a5952fa64f02.jpg" alt="title of video">
+				<div class="description">
+					<h1>Fight Club</h1>
+					<h2>Length</h2>
+					<p>125min</p>
+					<h2>Release Date</h2>
+					<p>October 32 2029</p>
+					<h2>Director</h2>
+					<p>TobyDoby Lemon</p>
+					<h2>Box Office</h2>
+					<p>&#36;20 Million</p>
+					<a onclick="launchTrailer('https://www.youtube.com/embed/WNhH00OIPP0')">Play Trailer</a>
+				</div>
+			</div>
+			-->
+			<!-- example for TV instance
+			<div class="content">
+				<img class="poster" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSd0NVxLFYMRfhJIjryxq6G-rWYQiqUnTf7bZmjwIgWA8HNUHXo" alt="title of video">
+				<div class="description">
+					<h1>Rick and Morty</h1>
+					<h2>Network</h2>
+					<p>Adult Swim</p>
+					<h2>First Episode</h2>
+					<p>February 31 2001</p>
+					<h2>Last Episode</h2>
+					<p>On going</p>
+					<h2>Number of Seasons</h2>
+					<p>3</p>
+					<h2>Number of Episodes</h2>
+					<p>22</p>
+					<a onclick="launchTrailer('https://www.youtube.com/embed/WNhH00OIPP0')">Play Trailer</a>
+				</div>
+			</div>
+			-->
+			{content_goes_here}
+		</div>
+		<div id="video-box">
+			<!-- trailer video format
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/WNhH00OIPP0" frameborder="0" allowfullscreen></iframe>
+			-->
+		</div>
+	</body>
+</html>
+'''
+
+movie_title_content = '''
+<div class="content">
+	<img class="poster" src="{poster_url}" alt="{movie_title}">
+	<div class="description">
+		<h1>{movie_title}</h1>
+		<h2>Length</h2>
+		<p>{duration}</p>
+		<h2>Release Date</h2>
+		<p>{release_date}</p>
+		<h2>Director</h2>
+		<p>{director}</p>
+		<h2>Box Office</h2>
+		<p>{box_office}</p>
+		<a onclick="launchTrailer('{trailer_url}')">Play Trailer</a>
+	</div>
+</div>
+'''
+
+tv_title_content = '''
+<div class="content">
+	<img class="poster" src="{poster_url}" alt="{tv_title}">
+	<div class="description">
+		<h1>{tv_title}</h1>
+		<h2>Network</h2>
+		<p>{network}</p>
+		<h2>First Episode</h2>
+		<p>{first_episode}</p>
+		<h2>Last Episode</h2>
+		<p>{last_episode}</p>
+		<h2>Number of Seasons</h2>
+		<p>{number_of_seasons}</p>
+		<h2>Number of Episodes</h2>
+		<p>{number_of_episodes}</p>
+		<a onclick="launchTrailer('{trailer_url}')">Play Trailer</a>
+	</div>
+</div>
+'''
+
+def create_content(video):
+#spits out html content for movies and TVs
+	content = ''
+	for vid in video:
+		if vid.vid_type == 'movie':
+			content += movie_title_content.format(
+				poster_url = vid.image_url,
+				movie_title = vid.title,
+				duration = vid.duration,
+				release_date = vid.release_date,
+				director = vid.director,
+				box_office = vid.box_office,
+				trailer_url = vid.trailer_url
+			)
+		elif vid.vid_type == 'tv':
+			content += tv_title_content.format(
+				poster_url = vid.image_url,
+				tv_title = vid.title,
+				network = vid.tv_network,
+				first_episode = vid.first_episode,
+				last_episode = vid.last_episode,
+				number_of_seasons = vid.num_seasons,
+				number_of_episodes = vid.num_episodes,
+				trailer_url = vid.trailer_url
+			)
+		else:
+			None
+	return content
+
+def create_html(video):
+	#creates movies.html file
+	output_file = open('movies.html', 'w')
+
+	#addes movies and TVs content to the body of html
+	output_html = html_body.format(content_goes_here = create_content(video))
+
+	#writes head and body of html to movies.html
+	output_file.write(html_head + output_html)
+	output_file.close()
+	
+	url = os.path.abspath(output_file.name)
+	webbrowser.open('file://' + url, new=2)
--- entertainment_center.py
+++ entertainment_center.py
@@ -1,4 +1,5 @@
 import media
+import constructor
 
 '''
 Movie class format (self, title, image_url, trailer_url, box_office, release_date, duration, director)
@@ -9,24 +10,26 @@ TV class format    (self, title, image_url, trailer_url, num_seasons, num_episod
 '''
 #MOVIE
 the_legend_of_1900 = media.Movie('movie', 'The Legend of 1900', 'https://upload.wikimedia.org/wikipedia/en/thumb/5/50/Leggenda_pianista.jpg/220px-Leggenda_pianista.jpg',
-								 'https://www.youtube.com/watch?v=2uf-LDlZMFE', '$259,127', 'October 28 1998', '125 minutes', 'Giuseppe Tornatore')
+								 'https://www.youtube.com/embed/2uf-LDlZMFE', '$259,127', 'October 28 1998', '125 minutes', 'Giuseppe Tornatore')
 
 dr_strangelove = media.Movie('movie', 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb',
 							 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e6/Dr._Strangelove_poster.jpg/220px-Dr._Strangelove_poster.jpg',
-							 'https://www.youtube.com/watch?v=1gXY3kuDvSU', '$9.4 million', 'January 29 1964', '94 minutes', 'Stanley Kubrick')
+							 'https://www.youtube.com/embed/1gXY3kuDvSU', '$9.4 million', 'January 29 1964', '94 minutes', 'Stanley Kubrick')
 
 cloud_atlas = media.Movie('movie', 'Cloud Atlas', 'https://upload.wikimedia.org/wikipedia/en/2/20/Cloud_Atlas_Poster.jpg',
-						  'https://www.youtube.com/watch?v=hWnAqFyaQ5s', '$130.5 million', 'October 26 2012', '171 minutes',
+						  'https://www.youtube.com/embed/hWnAqFyaQ5s', '$130.5 million', 'October 26 2012', '171 minutes',
 						  'Lana Wachowski, Tom Tykwer, Andy wachowski')
 
 #TV
-dexter = media.TV('tv', 'Dexter', 'http://www.gstatic.com/tv/thumb/tvbanners/9707531/p9707531_b_v8_aa.jpg', 'https://www.youtube.com/watch?v=YQeUmSD1c3g',
+dexter = media.TV('tv', 'Dexter', 'http://www.gstatic.com/tv/thumb/tvbanners/9707531/p9707531_b_v8_aa.jpg', 'https://www.youtube.com/embed/YQeUmSD1c3g',
 				  8, 96, 'October 1 2006', 'September 22 2013', "Showtime")
 
 rick_and_morty = media.TV('tv', 'Rick and Morty', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI30BIvWf2gh_Qr3-ksjb83DrCbivewBoHhzM3Hww5HB7DSuLa8Xybfg',
-						  'https://www.youtube.com/watch?v=WNhH00OIPP0', 3, 22, 'December 2 2013', 'present', 'Adult Swim')
+						  'https://www.youtube.com/embed/WNhH00OIPP0', 3, 22, 'December 2 2013', 'present', 'Adult Swim')
 
-narcos = media.TV('tv', 'Narcos', 'http://www.gstatic.com/tv/thumb/tvbanners/12985822/p12985822_b_v8_ab.jpg', 'https://www.youtube.com/watch?v=U7elNhHwgBU',
+narcos = media.TV('tv', 'Narcos', 'http://www.gstatic.com/tv/thumb/tvbanners/12985822/p12985822_b_v8_ab.jpg', 'https://www.youtube.com/embed/U7elNhHwgBU',
 				  2, 20, 'August 28 2015', 'present', 'Netflix')
 
 videos = [the_legend_of_1900, dr_strangelove, cloud_atlas, dexter, rick_and_morty, narcos]
+
+constructor.create_html(videos)
